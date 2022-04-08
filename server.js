161437abd5eabedb1269fc321f556aac24e6feaf
Erhan <demirci.erhan@windowslive.com>
2022-04-09 00:57:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+
+
+app.get("/youtube/callback", (req, res) => {
+    console.log('here', req.body, req.query, req.params, req.headers)
+    let query = req.query
+    if('hub.challenge' in query){
+        let channelID = query['hub.topic'].split('=').pop()
+        /*if(!bot.youtube.pendingVerify.has(channelID)) {
+            console.log('Invalid youtube hook sent to me', channelID, req.headers)
+            return res.sendStatus(401)
+        }*/
+        bot.youtube.lease.set(channelID, Date.now() + (query['hub.lease_seconds'] * 1000))
+        bot.youtube.pendingVerify.delete(channelID)
+        return res.status(202).send(query['hub.challenge'])
+    }
+})
+
+app.post('/youtube/callback', (req, res) => {
+    console.log('here post', req.body, req.query, req.params, req.headers)
+
+    let hmac = generateHMAC(req.query['hub.challenge'], 'very_secret', "sha1")
+})
+
+function generateHMAC(hmac, secret, algo = 'sha256'){
+    return `${algo}=${crypto.createHmac(algo, secret).update(hmac).digest('hex')}`
+}
  
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
+
  
 // Start the server
 const PORT = process.env.PORT || 8080;
@@ -16,9 +37,6 @@ app.listen(PORT, () => {
 });
 
 
-var callback = function () {
-  console.log("10 seconds later...");
-};
 
 
 
