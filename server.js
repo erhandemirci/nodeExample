879400134d69db05a9d34329ab9b9c7d591225e7
Erhan <demirci.erhan@windowslive.com>
2022-04-09 01:13:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,32 @@ const app = express();
         res.send('im the about page!');
     });
 
+
+router.get("/youtube/callback", (req, res) => {
+    console.log('here', req.body, req.query, req.params, req.headers)
+    let query = req.query
+    if('hub.challenge' in query){
+        let channelID = query['hub.topic'].split('=').pop()
+        /*if(!bot.youtube.pendingVerify.has(channelID)) {
+            console.log('Invalid youtube hook sent to me', channelID, req.headers)
+            return res.sendStatus(401)
+        }*/
+        bot.youtube.lease.set(channelID, Date.now() + (query['hub.lease_seconds'] * 1000))
+        bot.youtube.pendingVerify.delete(channelID)
+        return res.status(202).send(query['hub.challenge'])
+    }
+})
+
+router.post('/youtube/callback', (req, res) => {
+    console.log('here post', req.body, req.query, req.params, req.headers)
+
+    let hmac = generateHMAC(req.query['hub.challenge'], 'very_secret', "sha1")
+})
+
+function generateHMAC(hmac, secret, algo = 'sha256'){
+    return `${algo}=${crypto.createHmac(algo, secret).update(hmac).digest('hex')}`
+}
+
     // apply the routes to our application
     app.use('/', router);
 
