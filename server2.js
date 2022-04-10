1890c70680cf8364b6cfd3299e18f9721fb62e74
Erhan <demirci.erhan@windowslive.com>
2022-04-10 23:31:18 +0300

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,4 +1,14 @@
 var pubSubHubbub = require("pubsubhubbub");
+
+
+var options = {
+  callback: "https://youtubenotificationserverapp.herokuapp.com/youtube/callback",
+  topic: "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UC1zAttFQKikWoKH3Vb39ETA",
+  mode: "subscribe",
+  method: "POST",
+  
+};
+
 var pubSubSubscriber = pubSubHubbub.createServer(options);
 var topic = "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UC1zAttFQKikWoKH3Vb39ETA";
 var hub = "http://pubsubhubbub.appspot.com/";
@@ -7,9 +17,9 @@ pubSubSubscriber.on("subscribe", function(data){
     console.log(data.topic + " subscribed");
 });
 
-pubSubSubscriber.listen(port);
 
-pubsub.on("listen", function(){
+
+pubSubSubscriber.on("listen", function(){
     pubSubSubscriber.subscribe(topic, hub, function(err){
         if(err){
             console.log("Failed subscribing");
